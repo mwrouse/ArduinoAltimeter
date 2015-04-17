d0b8daafbfab5f751d8d4d1f09c73419251dd3c3
Michael Rouse <michael@michaelrouse.net>
2015-04-16 20:29:32 -0500

    B

--- altimeter_graph/application.windows32/source/altimeter_graph.pde
+++ altimeter_graph/application.windows32/source/altimeter_graph.pde
@@ -4,14 +4,18 @@ import processing.serial.*;
 Serial arduino;
 String x = "";
 String y = "";
-int xPos = 60;
-float oldX = 56;
+int xPos = 100;
+float oldX = 100;
 float oldY = height;
 boolean graphing = true;
 
+int MODIFIER = 100;
+
+int MAX_ALT = 1524; // Max altitde in meters of the graph
+
 void setup()
 {
-   size(700, 600);
+   size(1000, 900);
    frame.setTitle("Rocket Flight Data");
    
    println(Serial.list());
@@ -32,15 +36,71 @@ void setup()
 
 void draw()
 {
+    textSize(36);
     //background(255);
-   line(50, (height-50), width, (height-50));
-   line(50, 0, 50, (height-50)); 
-  stroke(126);
-   //if (x!= null && y!=null){
-    //text(x, 10, 130);
-    //fill(0, 102, 153);
-    //text(y, 10, 80);
-   // fill(0, 102, 153);
+   line(MODIFIER, (height-MODIFIER), width, (height-MODIFIER));
+   line(MODIFIER, 0, MODIFIER, (height-MODIFIER)); 
+    stroke(126);
+
+    text("Time (s)", (width/2)-50, height-10);
+    fill(0, 102, 153);
+    textSize(36);
+    
+    text ("A", 10, (height/2) - 50);
+    text ("L", 10, (height/2) - 20);
+    text ("T", 10, (height/2) + 10);
+    fill(0, 102, 153);
+   
+   
+   textSize(15); 
+   
+   // Label the altitude axis
+   float value = MAX_ALT / 8.0;
+   float y;
+   int mody;
+   for (int i = 0; i < 8; i++)
+   {
+     
+     y = map(value * i, 0, MAX_ALT, 0, height - MODIFIER );
+     line(MODIFIER - 10, y, MODIFIER, y);
+     
+
+     if (i ==0)
+     {
+       text(MAX_ALT, 45, y + 20);
+     }else{
+       
+       if ((value * (8-i)) < 1000.0)
+         mody = 55;
+        else
+          mody = 45;
+          
+       text(int(value * (8-i)), mody, y + 7);
+     }
+   }
+    
+   
+   // Label the time axis
+   
+   for (int i = 10; i <= 100; i+=10)
+   {
+     
+     float xPos2 = map(i, 0, 100, 0, width - MODIFIER );
+     line (MODIFIER + xPos2, (height - MODIFIER), MODIFIER + xPos2, (height - MODIFIER) + 10);
+     
+     text(i, xPos2 + MODIFIER - 7, (height - MODIFIER) + 25);
+   }
+   
+   // Label 0
+   line(MODIFIER - 10, (height - MODIFIER), MODIFIER, (height - MODIFIER));
+   line(MODIFIER, (height - MODIFIER), MODIFIER, (height - MODIFIER) + 10);
+   text("0", MODIFIER - 15, (height - MODIFIER) + 15);
+   
+   
+   // Fill in all lines and text
+   fill(0, 102, 153);
+   stroke(126);
+   
    
 }
 
@@ -48,17 +108,18 @@ void serialEvent(Serial arduino)
 {
   y = arduino.readStringUntil('\n');
  
+  float value = map(0.25, 0, 100, 0, width - MODIFIER);
   
   if (y != null && graphing)
   {
       y = trim(y);
       
       float inY = float(y);
-
-      inY = map(inY, 0, 20, 00, height - 50);
+      
+      inY = map(inY, 0, MAX_ALT, 0, height - MODIFIER);
       
       stroke(127,34,255);
-      line(oldX, (height - oldY) - 60, xPos, (height - inY)-60);
+      line(oldX, (height - oldY) - MODIFIER - 10, xPos, (height - inY)-MODIFIER - 10);
        
       oldX = xPos;
       oldY = inY;
--- altimeter_graph/application.windows64/source/altimeter_graph.java
+++ altimeter_graph/application.windows64/source/altimeter_graph.java
@@ -22,14 +22,18 @@ public class altimeter_graph extends PApplet {
 Serial arduino;
 String x = "";
 String y = "";
-int xPos = 60;
-float oldX = 56;
+int xPos = 100;
+float oldX = 100;
 float oldY = height;
 boolean graphing = true;
 
+int MODIFIER = 100;
+
+int MAX_ALT = 1524; // Max altitde in meters of the graph
+
 public void setup()
 {
-   size(700, 600);
+   size(1000, 900);
    frame.setTitle("Rocket Flight Data");
    
    println(Serial.list());
@@ -50,15 +54,71 @@ public void setup()
 
 public void draw()
 {
+    textSize(36);
     //background(255);
-   line(50, (height-50), width, (height-50));
-   line(50, 0, 50, (height-50)); 
-  stroke(126);
-   //if (x!= null && y!=null){
-    //text(x, 10, 130);
-    //fill(0, 102, 153);
-    //text(y, 10, 80);
-   // fill(0, 102, 153);
+   line(MODIFIER, (height-MODIFIER), width, (height-MODIFIER));
+   line(MODIFIER, 0, MODIFIER, (height-MODIFIER)); 
+    stroke(126);
+
+    text("Time (s)", (width/2)-50, height-10);
+    fill(0, 102, 153);
+    textSize(36);
+    
+    text ("A", 10, (height/2) - 50);
+    text ("L", 10, (height/2) - 20);
+    text ("T", 10, (height/2) + 10);
+    fill(0, 102, 153);
+   
+   
+   textSize(15); 
+   
+   // Label the altitude axis
+   float value = MAX_ALT / 8.0f;
+   float y;
+   int mody;
+   for (int i = 0; i < 8; i++)
+   {
+     
+     y = map(value * i, 0, MAX_ALT, 0, height - MODIFIER );
+     line(MODIFIER - 10, y, MODIFIER, y);
+     
+
+     if (i ==0)
+     {
+       text(MAX_ALT, 45, y + 20);
+     }else{
+       
+       if ((value * (8-i)) < 1000.0f)
+         mody = 55;
+        else
+          mody = 45;
+          
+       text(PApplet.parseInt(value * (8-i)), mody, y + 7);
+     }
+   }
+    
+   
+   // Label the time axis
+   
+   for (int i = 10; i <= 100; i+=10)
+   {
+     
+     float xPos2 = map(i, 0, 100, 0, width - MODIFIER );
+     line (MODIFIER + xPos2, (height - MODIFIER), MODIFIER + xPos2, (height - MODIFIER) + 10);
+     
+     text(i, xPos2 + MODIFIER - 7, (height - MODIFIER) + 25);
+   }
+   
+   // Label 0
+   line(MODIFIER - 10, (height - MODIFIER), MODIFIER, (height - MODIFIER));
+   line(MODIFIER, (height - MODIFIER), MODIFIER, (height - MODIFIER) + 10);
+   text("0", MODIFIER - 15, (height - MODIFIER) + 15);
+   
+   
+   // Fill in all lines and text
+   fill(0, 102, 153);
+   stroke(126);
+   
    
 }
 
@@ -66,17 +126,18 @@ public void serialEvent(Serial arduino)
 {
   y = arduino.readStringUntil('\n');
  
+  float value = map(0.25f, 0, 100, 0, width - MODIFIER);
   
   if (y != null && graphing)
   {
       y = trim(y);
       
       float inY = PApplet.parseFloat(y);
-
-      inY = map(inY, 0, 20, 00, height - 50);
+      
+      inY = map(inY, 0, MAX_ALT, 0, height - MODIFIER);
       
       stroke(127,34,255);
-      line(oldX, (height - oldY) - 60, xPos, (height - inY)-60);
+      line(oldX, (height - oldY) - MODIFIER - 10, xPos, (height - inY)-MODIFIER - 10);
        
       oldX = xPos;
       oldY = inY;
